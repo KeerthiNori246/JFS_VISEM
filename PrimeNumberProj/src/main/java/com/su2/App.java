0d9c499aa7163d51f17ffd306eaed713c24309e2
keerthi-nori246 <246throwawayacc@gmail.com>
2025-12-31 09:46:54 +0530

    More

---
+++ PrimeNumberProj/src/main/java/com/su2/App.java
@@ -0,0 +1,23 @@
+package com.su2;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(checkPrime(n));
+    }
+	
+	public static boolean checkPrime(int n){
+		if(n==1) return true;
+		for(int i=2; i<=Math.sqrt(n); i++){
+			if(n%i == 0) return false;
+		}
+		return true;
+	}
+}
--- ps1/script.js
+++ ps1/script.js
@@ -1,31 +1,33 @@
-const personSelect = document.getElementById("personSelect");
-const profileCard = document.getElementById("profileCard");
-const profileName = document.getElementById("profileName");
-const profileImage = document.getElementById("profileImage");
+function showDetails() {
+    var select = document.getElementById("nameSelect").value;
+    var photo = document.getElementById("photo");
+    var infoText = document.getElementById("infoText");
 
-const profiles = {
-    john: {
-        name: "John Doe",
-        img: "https://randomuser.me/api/portraits/men/32.jpg"
-    },
-    keerthi: {
-        name: "Keerthi Nori",
-        img: "./images/keerthi.jpg"
-    },
-    mike: {
-        name: "Mike Johnson",
-        img: "https://randomuser.me/api/portraits/men/52.jpg"
+    if (select === "") {
+        photo.src = "";
+        infoText.innerText = "Please select a person.";
+        return;
     }
-};
 
-personSelect.addEventListener("change", function () {
-    const value = this.value;
+    var data = {
+        guido: {
+            img: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Guido_van_Rossum_OSCON_2006.jpg",
+            info: "Guido van Rossum is the creator of Python."
+        },
+        dennis: {
+            img: "https://upload.wikimedia.org/wikipedia/commons/3/35/Dennis_Ritchie_2011.jpg",
+            info: "Dennis M. Ritchie created the C programming language."
+        },
+        james: {
+            img: "https://upload.wikimedia.org/wikipedia/commons/e/e5/James_Gosling_Oct_2008.jpg",
+            info: "James Gosling is the creator of Java."
+        },
+        stroustrup: {
+            img: "https://upload.wikimedia.org/wikipedia/commons/5/53/BjarneStroustrup.jpg",
+            info: "Bjarne Stroustrup created C++."
+        }
+    };
 
-    if (value && profiles[value]) {
-        profileName.textContent = profiles[value].name;
-        profileImage.src = profiles[value].img;
-        profileCard.style.display = "block";
-    } else {
-        profileCard.style.display = "none";
-    }
-});
+    photo.src = data[select].img;
+    infoText.innerText = data[select].info;
+}
